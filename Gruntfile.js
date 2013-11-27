f329fb6a5d2241eb99a263aed5c56d25eba1b182
Jong-Ha Ahn <onpissoft@gmail.com>
2013-11-27 09:27:33 +0900

    change name for jquery plugin repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('jquery.emailinput.json'),
+    pkg: grunt.file.readJSON('emailinput.jquery.json'),
     name: 'jquery.emailinput',
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
